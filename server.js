304aaa18c78fba38f3ac4af11de9f3b1ab3955a5
jimitheworm <whole-lotta-love>
2018-04-13 00:35:44 +0300

    3

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
+var log = require('./logger')(module);
+var db = require('./db');
+db.connect();
+
 var User = require('./user');
 
 function run (){
-    var v = new User('v');
+    var v = new User('vqw');
     var p = new User('p');
 
     p.hello(v);
+    log(1, 2);
 }
 
 if (module.parent) {
     exports.run = run;
 } else {
+    console.log('server');
     run();
 }
